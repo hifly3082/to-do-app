5ea2aa88758d66842c0ea0c8f8642a64fbe10da3
mark.avvakumov <maavvakumov@gmail.com>
2024-01-09 14:58:59 +0700

    Update sample data usage

--- src/utilities/helpers.ts
+++ src/utilities/helpers.ts
@@ -4,6 +4,24 @@ export const generateId = (): string => {
   return `id-${Date.now()}-${Math.floor(Math.random() * 1000)}`
 }
 
+export const getRandomBoolean = () => Math.random() < 0.5
+
+export const getRandomDate = () => {
+  const currentDate = new Date()
+  const randomDays = Math.floor(Math.random() * 365)
+  const randomHours = Math.floor(Math.random() * 24)
+  const randomMinutes = Math.floor(Math.random() * 60)
+  currentDate.setDate(currentDate.getDate() + randomDays)
+  currentDate.setHours(randomHours, randomMinutes)
+
+  return currentDate.toISOString()
+}
+
+export const getRandomFromArray = <T>(array: T[]): T => {
+  const randomIndex = Math.floor(Math.random() * array.length)
+  return array[randomIndex]
+}
+
 export const calculateCompletionPercentage = (todos: Todo[]) => {
   if (todos.length === 0) return 0
 
