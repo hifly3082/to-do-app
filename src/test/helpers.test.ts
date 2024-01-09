6ec84a7cbf7cacba8d25fdf8bc3fe5443206ea5e
mark.avvakumov <maavvakumov@gmail.com>
2024-01-09 13:03:42 +0700

    Add coverage and history

--- src/test/helpers.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from 'vitest'
-
-import { calculateCompletionPercentage } from '../utilities/helpers'
-import { sampleData } from '../ui/components/todolist/sampleData'
-
-test('calculateCompletionPercentage', () => {
-  expect(calculateCompletionPercentage(sampleData)).toBe(25)
-})
